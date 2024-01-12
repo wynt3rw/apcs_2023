0fd2e3beea8d9b6f5015ea2cf0e6c965449b1ff8
wynt3rw <wynterwilliams25@marlborough.org>
2024-01-11 17:45:17 -0800

    updated unit ten assignment

--- unit10/PersonStats.java
+++ unit10/PersonStats.java
@@ -1,89 +1,79 @@
 import java.util.ArrayList;
 
-public class PersonStats{
-    public static double averagePets(ArrayList<Person> personList){
+public class PersonStats {
+    public static double averagePets(ArrayList<Person> personList) {
         int totalPets = 0;
-        
-        for(Person person : personList){
+
+        for (Person person : personList) {
             totalPets = totalPets + person.getPets();
         }
-        
+
         return (double) totalPets / personList.size();
     }
-    
-    public static double testAveragePets(ArrayList<Person> personList, double expected){
-        double result = averagePets(personList);
-        
-        for(Person person : personList){
-            System.out.println("Average Expected: " + expected + ". Average Result: " + result);
-        }
 
-        if(result == expected){
+    public static void testAveragePets(ArrayList<Person> personList, double expected) {
+        double result = averagePets(personList);
+
+        System.out.println("Average Expected: " + expected + ". Average Result: " + result);
+
+        if (result == expected) {
             System.out.println("Valid!");
-        }else{
+        } else {
             System.out.println("Invalid.");
         }
-        
-        return result;     
     }
-    
-    public static String mostPets(ArrayList<Person> personList){
+
+    public static String mostPets(ArrayList<Person> personList) {
         int maxPets = 0;
-        String withMostPets = " ";
-        
-        for(Person person : personList){
-            if(person.getPets() > maxPets){
+        String withMostPets = "";
+
+        for (Person person : personList) {
+            if (person.getPets() > maxPets) {
                 maxPets = person.getPets();
                 withMostPets = person.getName();
             }
         }
-        
+
         return withMostPets;
     }
-    
-    public static String testMostPets(ArrayList<Person> personList, String expected){
-        String result = mostPets(personList);
-         
-        for(Person person : personList){
-            System.out.println("Most Pets Expected: " + expected + ". Most Pets Result: " + result);
-        }
 
-        if(result == expected){
+    public static void testMostPets(ArrayList<Person> personList, String expected) {
+        String result = mostPets(personList);
+
+        System.out.println("Most Pets Expected: " + expected + ". Most Pets Result: " + result);
+
+        if (result.equals(expected)) {
             System.out.println("Valid!");
-        }else{
+        } else {
             System.out.println("Invalid.");
         }
-        
-        return result;   
     }
-    
-    public static ArrayList<Person> twoPets(ArrayList<Person> personList){
+
+    public static ArrayList<Person> twoPets(ArrayList<Person> personList) {
         ArrayList<Person> filterTwoPets = new ArrayList<>();
-        
-        for(Person person : personList){
-            if(person.getPets() >= 2){
+
+        for (Person person : personList) {
+            if (person.getPets() >= 2) {
                 filterTwoPets.add(person);
             }
         }
-        
+
         return filterTwoPets;
     }
-    
-    public static ArrayList<Person> testTwoPets(ArrayList<Person> personList, ArrayList<Person> expected){
+
+    public static void testTwoPets(ArrayList<Person> personList, ArrayList<Person> expected) {
         ArrayList<Person> result = twoPets(personList);
 
         System.out.println("Two Pets Expected: " + expected);
         System.out.println("Two Pets Result: " + result);
 
-        if (result.equals(expected)){
+        if (result.equals(expected)) {
             System.out.println("Valid!");
-        }else{
+        } else {
             System.out.println("Invalid.");
         }
+    }
 
-    return result;
-}
-    
     public static void main(String[] args) {
         ArrayList<Person> personList = new ArrayList<>();
         personList.add(new Person("Eliot", 2));
@@ -96,7 +86,7 @@ public class PersonStats{
         ArrayList<Person> expected = new ArrayList<>();
         expected.add(new Person("Eliot", 2));
         expected.add(new Person("Nirel", 4));
-        expected.add(new Person("Scarlettt", 3));
+        expected.add(new Person("Scarlett", 3));
 
         testTwoPets(personList, expected);
     }
