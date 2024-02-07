63d771433d7e79d094d01cafe92b0d5183a3ee50
wynt3rw <wynterwilliams25@marlborough.org>
2024-02-07 14:05:06 -0800

    updates to unit twelve assignment

--- unit12/ArrayInitialization.java
+++ unit12/ArrayInitialization.java
@@ -1,57 +1,90 @@
-public class ArrayInitialization{
-    private static void displayArray(String name, Object[][] array){
+public class ArrayInitialization {
+    private static void displayArrayA(String name, int[][] array){
         System.out.println(name + ":");
-        for(Object[] row : array){
-            for(Object element : row){
-                System.out.print(element + "\t");
+        for(int[] row : array){
+            for(int element : row){
+                System.out.print(element);
             }
             System.out.println();
         }
         System.out.println();
-    }    
+    }
     
-    public static void main(String[] args){
+    private static void displayArrayB(String name, Object[][] array) {
+        System.out.println(name + ":");
+        for (Object[] row : array){
+            for (Object element : row){
+                System.out.print(element);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     
+    private static void displayArrayC(String name, double[][] array){
+        System.out.println(name + ":");
+        for(double[] row : array){
+            for(double element : row){
+                System.out.println(element);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    private static void displayArrayD(String name, int[][] array){
+        System.out.println(name + ":");
+        for(int[] row : array){
+            for(int element : row){
+                System.out.print(element);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
         int[][] arrayA = new int[10][10];
-        for(int i = 0; i < 10; i = i + 1){
-        for(int x = 0; x < 10; x = x + 1){
-                if(i == x){
+        for (int i = 0; i < 10; i++) {
+            for (int x = 0; x < 10; x++) {
+                if (i == x) {
                     arrayA[i][x] = 1;
-                } else{
+                } else {
                     arrayA[i][x] = 0;
                 }
             }
         }
-    
+
         String[][] arrayB = {
-            {"Gold", "Silver", "Sapphire"},
-            {"Monitor", "Microphone", "Keyboard"},
-            {"Pencil", "Pen", "Highlighter"}
+                {"Gold", "Silver", "Sapphire"},
+                {"Monitor", "Microphone", "Keyboard"},
+                {"Pencil", "Pen", "Highlighter"}
         };
-    
+
         double[][] arrayC = new double[5][5];
         double valueC = 11.0;
-    
-        for(int i = 0; i < 5; i = i + 1){
-            for(int x = 0; x < 5; x = x + 1){
+
+        for (int i = 0; i < 5; i++) {
+            for (int x = 0; x < 5; x++) {
                 arrayC[i][x] = valueC;
                 valueC = valueC + 1;
             }
         }
-        
+
         int[][] arrayD = new int[6][5];
         int valueD = 101;
-        
-        for(int i = 0; i < 6; i = i + 1){
-            for(int x = 0; x < 5; x = x + 1){
+
+        for (int i = 0; i < 6; i++) {
+            for (int x = 0; x < 5; x++) {
                 arrayD[i][x] = valueD;
                 valueD = valueD + 1;
             }
         }
-        
-        displayArray("Array A", arrayA);
-        displayArray("Array B", arrayB);
-        displayArray("Array C", arrayC);
-        displayArray("Array D", arrayD);
+
+        displayArrayA("Array A", arrayA);
+        displayArrayB("Array B", arrayB);
+        displayArrayC("Array C", arrayC);
+        displayArrayD("Array D", arrayD);
     }
 }
