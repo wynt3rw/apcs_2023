3faef48c74a8093428a9a75873b2bfe065e62b50
wynt3rw <wynterwilliams25@marlborough.org>
2024-02-08 22:02:44 -0800

    platformer game base

--- project/Platformer.java
+++ project/Platformer.java
@@ -93,6 +93,23 @@ public class Platformer extends PApplet {
         }
     }
     
+    public class Platform {
+    int x, y, width, height;
+
+     public  Platform(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+     public void display() {
+        fill(0, 0, 255);
+        rect(x, y, width, height);
+    }
+}
+    
+    
     public static void main(String[] args) {
         PApplet.main("Platformer");
     }
